63914857b670c62b2ef25616f3db846e6e3454c2
Milan Gavrilov <Milan Gavrilov@MILAN-GAVRILOV>
2025-07-30 15:42:15 +0200

    Dodao handler za komandu /iskljucenja, kada se pozove, prikazuje sva iskljucenja iz unapred popunjenog json dokumenta

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import os
 import telebot
 from dotenv import load_dotenv
@@ -11,6 +12,19 @@ bot = telebot.TeleBot(BOT_TOKEN)
 def send_welcome(message):
     bot.reply_to(message, 'Welcome!')
 
+
+@bot.message_handler(commands=['iskljucenja'])
+def iskljucenja(message):
+    with open('iskljucenja.json', 'r', encoding='utf-8') as f:
+        iskljucenja = json.load(f)
+    # res = ''
+    # for i in iskljucenja:
+    #     res = res + f'{i['opstina']}: {i['vreme_od']} - {i['vreme_do']} | {i['ulice']}' + '\n'
+    # bot.reply_to(message, res)
+    bot.reply_to(message, 'Današnja najavljena isključenja za ogranak: Novi Sad')
+    for i in iskljucenja:
+        bot.reply_to(message, f'{i['opstina']}: {i['vreme_od']} - {i['vreme_do']} | {i['ulice']}')
+    # bot.reply_to(message, message.text[13:])
 
 @bot.message_handler(func=lambda msg: True)
 def echo(message):
