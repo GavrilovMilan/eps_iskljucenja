dd4d2ff67f2a1b988821a82f3404d1104a310e50
GavrilovMilan <milangavrilov01@gmail.com>
2025-07-30 22:55:21 +0200

    Malo ispeglan kod. Napravljena funkcija za pretvaranje latinice u cirilicu. Napravljen bazicni filter za ispisivanje planiranog iskljucenja struje na osnovu unetog teksta nakon komande /iskljucenje.

--- main.py
+++ main.py
@@ -8,6 +8,33 @@ BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
+def lat_to_cir(tekst):
+    mapa = {
+        'nj': 'њ', 'dž': 'џ', 'lj': 'љ',
+        'NJ': 'Њ', 'DŽ': 'Џ', 'LJ': 'Љ',
+        'Nj': 'Њ', 'Dž': 'Џ', 'Lj': 'Љ',
+        'nJ': 'Њ', 'dŽ': 'Џ', 'lJ': 'Љ',
+        'a': 'а', 'b': 'б', 'v': 'в', 'g': 'г', 'd': 'д', 'đ': 'ђ',
+        'e': 'е', 'ž': 'ж', 'z': 'з', 'i': 'и', 'j': 'ј', 'k': 'к',
+        'l': 'л', 'm': 'м', 'n': 'н', 'o': 'о', 'p': 'п', 'r': 'р',
+        's': 'с', 't': 'т', 'ć': 'ћ', 'u': 'у', 'f': 'ф', 'h': 'х',
+        'c': 'ц', 'č': 'ч', 'š': 'ш',
+        'A': 'А', 'B': 'Б', 'V': 'В', 'G': 'Г', 'D': 'Д', 'Đ': 'Ђ',
+        'E': 'Е', 'Ž': 'Ж', 'Z': 'З', 'I': 'И', 'J': 'Ј', 'K': 'К',
+        'L': 'Л', 'M': 'М', 'N': 'Н', 'O': 'О', 'P': 'П', 'R': 'Р',
+        'S': 'С', 'T': 'Т', 'Ć': 'Ћ', 'U': 'У', 'F': 'Ф', 'H': 'Х',
+        'C': 'Ц', 'Č': 'Ч', 'Š': 'Ш'
+    }
+
+    for lat, cir in [('nj', 'њ'), ('dž', 'џ'), ('lj', 'љ'),
+                     ('NJ', 'Њ'), ('DŽ', 'Џ'), ('LJ', 'Љ'),
+                     ('Nj', 'Њ'), ('Dž', 'Џ'), ('Lj', 'Љ'),
+                     ('nJ', 'Њ'), ('dŽ', 'Џ'), ('lJ', 'Љ')]:
+        tekst = tekst.replace(lat,cir)
+
+    return ''.join(mapa.get(c,c) for c in tekst)
+
+
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
     bot.reply_to(message, 'Welcome!')
@@ -15,16 +42,27 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['iskljucenja'])
 def iskljucenja(message):
-    with open('iskljucenja.json', 'r', encoding='utf-8') as f:
+    with open('iskljucenja/iskljucenja.json', 'r', encoding='utf-8') as f:
         iskljucenja = json.load(f)
-    # res = ''
-    # for i in iskljucenja:
-    #     res = res + f'{i['opstina']}: {i['vreme_od']} - {i['vreme_do']} | {i['ulice']}' + '\n'
-    # bot.reply_to(message, res)
-    bot.reply_to(message, 'Današnja najavljena isključenja za ogranak: Novi Sad')
-    for i in iskljucenja:
-        bot.reply_to(message, f'{i['opstina']}: {i['vreme_od']} - {i['vreme_do']} | {i['ulice']}')
-    # bot.reply_to(message, message.text[13:])
+
+    if len(message.text) > 13:
+        # Spaja više isključenja u jednu poruku (može nastati problem ukoliko je poruka duža od 4096 karaktera)
+        res = ''
+        print(message.text[13:])
+        for i in iskljucenja:
+            if lat_to_cir(message.text[13:]).upper() in i['ulice'].upper():
+                res = res + f"{i['opstina']}: {i['vreme_od']} - {i['vreme_do']} | {i['ulice']}" + '\n'
+        if res != '':
+            bot.reply_to(message, res)
+        else:
+            bot.reply_to(message, 'Nema najavljenih isključenja struje za zadati filter')
+    else:
+        bot.reply_to(message, f"Sva najavljena isključenja za današnji dan")
+        # Svako isključenje je jedna poruka
+        for i in iskljucenja:
+            bot.reply_to(message, f"{i['opstina']}: {i['vreme_od']} - {i['vreme_do']} | {i['ulice']}")
+
+        # bot.reply_to(message, message.text[13:])
 
 @bot.message_handler(func=lambda msg: True)
 def echo(message):
--- scraping.py
+++ scraping.py
@@ -4,42 +4,64 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 
 
-url = 'https://elektrodistribucija.rs/planirana-iskljucenja-srbija/NoviSad_Dan_0_Iskljucenja.htm'
+urls = ['https://elektrodistribucija.rs/planirana-iskljucenja-srbija/NoviSad_Dan_0_Iskljucenja.htm'#,
+        # 'https://elektrodistribucija.rs/planirana-iskljucenja-srbija/NoviSad_Dan_1_Iskljucenja.htm',
+        # 'https://elektrodistribucija.rs/planirana-iskljucenja-srbija/NoviSad_Dan_2_Iskljucenja.htm',
+        # 'https://elektrodistribucija.rs/planirana-iskljucenja-srbija/NoviSad_Dan_3_Iskljucenja.htm',
+        # 'https://elektrodistribucija.rs/planirana-iskljucenja-beograd/Dan_0_Iskljucenja.htm',
+        # 'https://elektrodistribucija.rs/planirana-iskljucenja-beograd/Dan_1_Iskljucenja.htm',
+        # 'https://elektrodistribucija.rs/planirana-iskljucenja-beograd/Dan_2_Iskljucenja.htm',
+        # 'https://elektrodistribucija.rs/planirana-iskljucenja-beograd/Dan_3_Iskljucenja.htm',
+        # 'https://elektrodistribucija.rs/planirana-iskljucenja-srbija/Kraljevo_Dan_0_Iskljucenja.htm',
+        # 'https://elektrodistribucija.rs/planirana-iskljucenja-srbija/Kraljevo_Dan_1_Iskljucenja.htm',
+        # 'https://elektrodistribucija.rs/planirana-iskljucenja-srbija/Kraljevo_Dan_2_Iskljucenja.htm',
+        # 'https://elektrodistribucija.rs/planirana-iskljucenja-srbija/Kraljevo_Dan_3_Iskljucenja.htm',
+        # 'https://elektrodistribucija.rs/planirana-iskljucenja-srbija/Kragujevac_Dan_0_Iskljucenja.htm',
+        # 'https://elektrodistribucija.rs/planirana-iskljucenja-srbija/Kragujevac_Dan_1_Iskljucenja.htm',
+        # 'https://elektrodistribucija.rs/planirana-iskljucenja-srbija/Kragujevac_Dan_2_Iskljucenja.htm',
+        # 'https://elektrodistribucija.rs/planirana-iskljucenja-srbija/Kragujevac_Dan_3_Iskljucenja.htm',
+        # 'https://elektrodistribucija.rs/planirana-iskljucenja-srbija/Nis_Dan_0_Iskljucenja.htm',
+        # 'https://elektrodistribucija.rs/planirana-iskljucenja-srbija/Nis_Dan_1_Iskljucenja.htm',
+        # 'https://elektrodistribucija.rs/planirana-iskljucenja-srbija/Nis_Dan_2_Iskljucenja.htm',
+        # 'https://elektrodistribucija.rs/planirana-iskljucenja-srbija/Nis_Dan_3_Iskljucenja.htm'
+        ]
 
 headers = {'User-Agent': 'Mozilla/5.0'}
-req = Request(url, headers=headers)
-page = urlopen(req)
-html = page.read().decode('utf-8')
-soup = BeautifulSoup(html, 'html.parser')
-# print(soup.prettify())
 
-# print(soup.get_text())
-# print(soup.find_all('tr'))
+for url in urls:
+    req = Request(url, headers=headers)
+    page = urlopen(req)
+    html = page.read().decode('utf-8')
+    soup = BeautifulSoup(html, 'html.parser')
+    # print(soup.prettify())
 
-raw_data = soup.find_all('tr')
-datum = re.search(r'за датум:\s*(\d{2}\.\d{2}\.\d{4}\.)', str(raw_data), re.IGNORECASE).group(1)
-# datum = datum.group(1)
-print(f'Datum: {datum}')
-iskljucenja = []
-for row in raw_data[2:]: # preskačemo naslove kolona
-    cols = row.find_all('td')
-    ogranak = cols[0].get_text(strip=True)
-    opstina = cols[1].get_text(strip=True)
-    vreme_od = cols[2].get_text(strip=True).split(' - ')[0]
-    vreme_do = cols[2].get_text(strip=True).split(' - ')[1]
-    ulice = cols[3].get_text(strip=True)
-    iskljucenje = {
-        'ogranak': ogranak,
-        'opstina': opstina,
-        'vreme_od': vreme_od,
-        'vreme_do': vreme_do,
-        'ulice': ulice
-    }
-    iskljucenja.append(iskljucenje)
+    # print(soup.get_text())
+    # print(soup.find_all('tr'))
 
-# for i in iskljucenja:
-#     print(f'{i['opstina']}: {i['vreme_od']} - {i['vreme_do']} | {i['ulice']}')
+    raw_data = soup.find_all('tr')
+    datum = re.search(r'за датум:\s*(\d{2}\.\d{2}\.\d{4}\.)', str(raw_data), re.IGNORECASE).group(1)
+    # datum = datum.group(1)
+    print(f'Datum: {datum}')
+    iskljucenja = []
+    for row in raw_data[2:]: # preskačemo naslove kolona
+        cols = row.find_all('td')
+        ogranak = cols[0].get_text(strip=True)
+        opstina = cols[1].get_text(strip=True)
+        vreme_od = cols[2].get_text(strip=True).split(' - ')[0]
+        vreme_do = cols[2].get_text(strip=True).split(' - ')[1]
+        ulice = cols[3].get_text(strip=True)
+        iskljucenje = {
+            'ogranak': ogranak,
+            'opstina': opstina,
+            'vreme_od': vreme_od,
+            'vreme_do': vreme_do,
+            'ulice': ulice
+        }
+        iskljucenja.append(iskljucenje)
 
-with open('iskljucenja.json', 'w', encoding='utf-8') as f:
-    json.dump(iskljucenja, f, ensure_ascii=False, indent=2)
+    # for i in iskljucenja:
+    #     print(f'{i['opstina']}: {i['vreme_od']} - {i['vreme_do']} | {i['ulice']}')
+
+    with open('iskljucenja/iskljucenja.json', 'w', encoding='utf-8') as f:
+        json.dump(iskljucenja, f, ensure_ascii=False, indent=2)
 
